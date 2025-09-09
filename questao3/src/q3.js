11aa3cee52f053078eade948620a6b70ec2fb02a
LuizEdu10s <luiz-souza.ls@academico.ifpb.edu.br>
2025-09-09 16:54:15 -0300

    Commit questa2 e questao3

---
+++ questao3/src/q3.js
@@ -0,0 +1,31 @@
+let sum = (vetor) => {
+    let num_vetor = 0;
+    for(let i of vetor){
+        num_vetor += i;
+    }
+    console.log(num_vetor);
+}
+
+let sumOdds = (vetor) => {
+    let subVetor = [];
+    let SomaT = 0;
+    for(let i of vetor){
+        if(i % 2 != 0){
+            subVetor.push(i)
+        }
+    }
+    for(let i of subVetor){
+        SomaT += i
+    }
+    console.log(SomaT)
+}
+
+let product = (vetor) => {
+    let num_vetor = 1;
+    for(let i of vetor){
+        num_vetor *= i;
+    }
+    console.log(num_vetor);
+}
+
+product([1,2,3,4,5,6])
